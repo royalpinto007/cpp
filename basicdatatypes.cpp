4eab1d19a4f44a271bda8b400427395f10041f55
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-03-26 07:33:47 +0530

    Add files via upload

---
+++ basicdatatypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    double d;
+    char c;
+    float f;
+
+    cin>>a>>d>>c>>f;
+    cout<<a<<" "<<d<<" "<<c<<" "<<f;
+}
---
+++ basictypecasting1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    char c;
+
+    a=19920;    //big number, unlike last question!
+    c=a;
+    cout<<c;
+}
