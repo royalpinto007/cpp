a6e67f978d7fff7f08654dd7a78e05b5cb9931e3
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-03-28 11:28:28 +0530

    Add files via upload

---
+++ datatypes2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double a=6/4;                           //int/int=int again 6/4 becomes 1
+    int b=6/4;
+    double c=a+b;
+    cout<<c;
+}
