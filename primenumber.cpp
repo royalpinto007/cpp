c475a86ba014f3f05637f43f312e96001a67d43f
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-03-29 07:10:14 +0530

    Add files via upload

---
+++ primenumber.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int d=2;
+    bool divided=false;
+
+    while (d<n){
+        if(n%d==0){
+            
+            divided=true;
+            break;
+        }
+        d++;
+    }
+    
+    if(divided){
+        cout<<"Not Prime"<<endl;
+    } 
+    else{ 
+        cout<<"Prime"<<endl;
+    
+    }
+    
+
+}
