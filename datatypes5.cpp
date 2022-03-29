6fc4f8778430289d9093c2702aefb06b83a68d67
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-03-29 07:07:39 +0530

    Add files via upload

---
+++ datatypes5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int var1=5;
+    int var2=6;
+
+    if((var2=1)==var1)                      // here var2 becomes 1 from 6 & condition is false, so it goes to else condition
+    {
+        cout<<var2;                         
+    }
+    else
+    {
+        cout<<(var2+1);                     // var2 is 1, therefore 1+1=2 will be the answer
+    }
+}
