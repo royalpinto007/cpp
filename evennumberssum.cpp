2ae0d27f325ac8ce008bc771dbf330c744b3179f
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-03-30 08:22:13 +0530

    Add files via upload

---
+++ evennumberssum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int sum=0, n, i=2;
+    cin>>n;
+
+    while(i<=n){
+        if(i%2==0){
+            sum=sum+i;
+
+        }
+        i++;
+
+    }
+    cout<<sum;
+}
