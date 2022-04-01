b5786fad017991b3c1873d9efd775d3eda817bea
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-01 17:40:43 +0530

    Add files via upload

---
+++ power.cpp
@@ -0,0 +1,15 @@
+//Assume 0 power 0 =1
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int x,n;
+    cin>>x>>n;
+
+    int ans;
+    ans=pow(x,n);                                                   //pow is used to find the power
+    cout<<ans;
+
+}
