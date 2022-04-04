79e7ded1c25f7de2d0ef250a36fbe88e9c80ab02
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-04 07:24:19 +0530

    Add files via upload

---
+++ pattern16.cpp
@@ -0,0 +1,39 @@
+/* 
+   1
+  23
+ 345
+4567
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i=1;
+    while(i<=n){
+        
+        int spaces=1;
+        while(spaces<=n-i){
+            cout<<" ";
+            spaces++;
+        }
+
+
+
+
+        int j=i;
+        int k=1;
+        while(k<=i){
+            cout<<j;
+            j++;
+            k++;
+            
+        }
+    cout<<endl;
+    i++;
+    }
+
+}
