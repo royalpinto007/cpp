6f82c2a47e5e3e0ff9380d88e056f6c2d4aa55bb
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-04 07:24:37 +0530

    Add files via upload

---
+++ pattern17.cpp
@@ -0,0 +1,21 @@
+/*
+1111
+222
+33
+4
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
