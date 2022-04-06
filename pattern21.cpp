e6b525426f8c9c7fa0c07d540bc9f8cbfb71a159
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-06 14:29:27 +0530

    Update pattern21.cpp

--- pattern21.cpp
+++ pattern21.cpp
@@ -16,7 +16,7 @@ int main(){
     while(i<=n){
         int j=1;
         while(j<=n){
-            char ch='A'+j-1;
+            char ch='A'+i-1;
             cout<<ch;
             j++;
         }
