fdb86e88dad148eb541b584fc8f29beaf95de4e9
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-11 21:38:45 +0530

    Add files via upload

---
+++ basicforloop4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+        if(j==1)
+            break;
+            cout<<j<<" ";
+    }
+}
+}
