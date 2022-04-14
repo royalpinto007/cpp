4cba89e292bde782d6160e465a7d15a5afa244b8
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-14 18:52:59 +0530

    Add files via upload

---
+++ scope5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=10;
+    while(a>5){
+        int a=1;
+        cout<<a<<" ";
+        a--;                        //infinite 1's will be printed
+    }
+}
