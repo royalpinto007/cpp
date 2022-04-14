b5d821f26da18943d3058b4ccbdc54ad84ed289c
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-14 18:52:43 +0530

    Add files via upload

---
+++ scope4.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0; i<3; i++){
+        cout<<i<<" ";
+    }
+  //  cout<<i<<endl;                   i wasnt declared in this scope
+}
