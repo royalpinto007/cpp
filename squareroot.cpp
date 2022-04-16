ce59485a670239646cd03d9a6493699879d42f57
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-16 20:50:26 +0530

    Add files via upload

---
+++ squareroot.cpp
@@ -0,0 +1,22 @@
+/*
+Given a number N, find its square root.
+You need to find and print only the integral part of square root of N.
+
+For eg. if number given is 18, answer is 4.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Write your code here
+    int n, ans = 0;
+    cin >> n;
+
+    while (ans * ans <= n)
+    {
+        ans++;
+    }
+    cout << ans - 1;
+}
