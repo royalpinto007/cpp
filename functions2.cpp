9b83ec9cb5cbf7da8c2c6de95481cf37445d47fe
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-19 22:54:39 +0530

    Add files via upload

---
+++ functions2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+void demo(int a, int b){
+    cout <<a<<" "<<b;
+}
+
+int main() {
+    int a = 5;
+    int b = 15; 
+    demo(a);                    //Compilation ERROR
+}
