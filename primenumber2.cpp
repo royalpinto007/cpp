ed6b306566cba03840f15232a8387a712cc4f7e5
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-20 19:25:11 +0530

    Add files via upload

---
+++ primenumber2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+bool isprime(int n){
+    int d=2;
+    while(d<n){
+        if(n%d==0){
+            return false;
+        }
+        d++;
+    }
+    return true;
+}
+
+int main(){
+
+    bool ans=isprime(30);
+    cout<<ans<<endl;
+    ans=isprime(31);
+    cout<<ans<<endl;
+}
