b4c06c479ea3f2d3a20bcf82324e1d7deb14f8ee
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-04-22 22:02:37 +0530

    Update and rename Arrays2.cpp to array2.cpp

---
+++ Arrays/array2.cpp
@@ -0,0 +1,26 @@
+// Array Sum
+// Given an array of length N, you need to find and print the sum of all elements of the array.
+  
+  
+  #include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<endl;
+    int input[99999];
+
+    for (int i=0; i<n;i++){
+        cin>>input[i];
+    }
+   int sum=0;
+    for(int i=0; i<n; i++){
+
+        sum=sum+input[i];
+
+    }
+    cout<<endl;
+        cout<<sum;
+}
+
