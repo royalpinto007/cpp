baa0ce30af62ed0fc57068e95d1ff82fd6b00c7f
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-05-14 12:13:16 +0530

    Add files via upload

---
+++ linearsearch.cpp
@@ -0,0 +1,46 @@
+/*
+Input-
+1)The first line contains an Integer 't' which denotes the number of test cases or queries to be run. 
+2)First line of each test case or query contains an integer 'N' representing the size of the array/list.
+3)Second line contains 'N' single space separated integers representing the elements in the array/list.
+4)Third line contains the value of X(integer to be searched in the given array/list.
+
+
+Output-
+Print the index at which X is present or -1, otherwise.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int linearSearch(int *arr, int n, int x)
+{
+    for(int i = 0 ; i < n ; i++) {
+        if(x == arr[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int *arr = new int[n];
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> arr[i];
+		}
+		int val;
+		cin >> val;
+		cout << linearSearch(arr, n, val) << endl;
+	}
+	return 0;
+}
