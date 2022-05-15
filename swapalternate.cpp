fc50fec4781babb9bb2c0a05e7e94b6a6f228831
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-05-15 07:59:43 +0530

    Add files via upload

---
+++ swapalternate.cpp
@@ -0,0 +1,49 @@
+/*
+
+You have been given an array/list(ARR) of size N. You need to swap every pair of alternate elements in the array/list.
+You don't need to print or return anything, just change in the input array itself.
+
+Sample Input-
+1
+6
+
+Output-
+1 3 5 6 4 2
+*/
+
+#include <iostream>
+using namespace std;
+
+void swapAlternate(int *arr, int size)
+{
+    for (int i = 0; i < size - 1; i = i + 2)
+    {
+
+        int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int size;
+        cin >> size;
+        int *arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            cin >> arr[i];
+        }
+        swapAlternate(arr, size);
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        delete[] arr;
+    }
+}
