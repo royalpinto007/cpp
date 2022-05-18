cc0fb87e20d1684ce3827668f00692e9c4d51cd6
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-05-18 15:13:27 +0530

    Add files via upload

---
+++ array3.cpp
@@ -0,0 +1,18 @@
+// Assume an integer takes 4 bytes and a pointer takes 8 bytes.
+
+// What is the Output?
+
+#include <iostream>
+using namespace std;
+
+void func(int a[]) {
+    cout << sizeof(a) << endl;
+}
+
+int main() {
+    int a[10];
+    func(a);
+}
+
+
+// Output- 8
