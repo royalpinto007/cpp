d54816767a272a322d91bb24672b9f5ce0cb8652
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-05-25 18:36:48 +0530

    Add files via upload

---
+++ Replace Character.cpp
@@ -0,0 +1,48 @@
+/*
+
+Given an input string S and two characters c1 and c2,
+you need to replace every occurrence of character c1 with character c2 in the given string.
+
+Input Format :
+Line 1 : Input String S
+Line 2 : Character c1 and c2 (separated by space)
+
+Output Format :
+Updated string
+
+*/
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void replaceCharacter(char input[], char c1, char c2)
+{
+    int count = 0;
+
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        count++;
+    }
+    int start = 0;
+    int end = count - 1;
+
+    while (start <= end)
+    {
+        if (input[start] == c1)
+        {
+
+            input[start] = c2;
+        }
+        start++;
+    }
+}
+
+int main()
+{
+    char input[1000000];
+    char c1, c2;
+    cin >> input;
+    cin >> c1 >> c2;
+    replaceCharacter(input, c1, c2);
+    cout << input << endl;
+}
