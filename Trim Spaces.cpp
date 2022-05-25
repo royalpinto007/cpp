30d44fa488107cc9d461b6f570674619c18effa5
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-05-25 18:39:51 +0530

    Add files via upload

---
+++ Trim Spaces.cpp
@@ -0,0 +1,52 @@
+/*
+
+Given an input string S that contains multiple words, you need to remove all the spaces present in the input string.
+There can be multiple spaces present after any word.
+Input Format :
+String S
+
+Output Format :
+Updated string
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void trimSpaces(char input[])
+{
+    int j = 0;
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        if (input[i] != ' ')
+        {
+            input[j] = input[i];
+            j++;
+        }
+    }
+    input[j] = '\0';
+}
+/*
+int count=0;
+{
+for(int i=0; input[i]!='\0'; i++){
+    count++;
+
+}
+cout<<count;
+}
+
+int j=0, k=count-1;
+while(j<k){
+    if(input[j]=='\0'){
+        continue;
+    }
+}
+*/
+int main()
+{
+    char input[1000000];
+    cin.getline(input, 1000000);
+    trimSpaces(input);
+    cout << input << endl;
+}
