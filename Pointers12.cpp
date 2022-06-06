e3adcee58e4b59592059bb21acdea0cb82d0f1c6
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-06-06 21:20:46 +0530

    Add files via upload

---
+++ Pointers12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// Assume memory address of variable ‘a’ is : 200 and a double variable is of size 8 bytes, what will be the output -
+int main()
+{
+    double a = 10.54;
+    double *d = &a;
+    d = d + 1;
+    cout << d << endl;
+}
