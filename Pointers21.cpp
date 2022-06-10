431b57cebb56d0dcb363d182ae9b882b01e45145
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-06-10 17:01:57 +0530

    Add files via upload

---
+++ Pointers21.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//Assume address of 0th index of array ‘b’ is 200. What is the output -
+int main()
+{
+    char b[] = "xyz";
+    char *c = &b[0];
+    cout << c << endl;          // xyz will be printed
+}
+
+//c stores the address of start of array b (and not of it’s values). 
+//So the entire array is printed when c is printed.
