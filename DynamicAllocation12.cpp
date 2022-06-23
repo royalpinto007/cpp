8a0064087ca0dc2dfe778235fb7379f98160513f
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-06-23 17:13:14 +0530

    Add files via upload

---
+++ DynamicAllocation12.cpp
@@ -0,0 +1,14 @@
+// Which statement(s) will give an error for the following code-
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int p = 5;
+    int const *q = &p;
+}
+
+// p++;
+// q++;
+// (*q)++;                  //answer
