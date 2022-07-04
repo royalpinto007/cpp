f9259010a85ff52b72653fb1d555c6f30283b441
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-05 00:28:04 +0530

    Update Trim Spaces.cpp

--- Character Arrays and 2D Arrays/Trim Spaces.cpp
+++ Character Arrays and 2D Arrays/Trim Spaces.cpp
@@ -1,6 +1,7 @@
 /*
 Given an input string S that contains multiple words, you need to remove all the spaces present in the input string.
 There can be multiple spaces present after any word.
+
 Input Format :
 String S
 
