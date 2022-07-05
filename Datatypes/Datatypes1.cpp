9c9d2a05f5d1011250f7b73c4c0dedb4d34b9091
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-06 01:37:23 +0530

    Update and rename datatypes1.cpp to Datatypes1.cpp

--- Datatypes/Datatypes1.cpp
+++ Datatypes/Datatypes1.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main(){
-    
     //Note- int/int=int
-
     int f;
     float g;                                //dont forget to enter it again :)
     cin>>f>>g;
@@ -12,7 +10,7 @@ int main(){
     int c=(5/9)*(f-32);                     // 5/9 is of the form int/int, 5/9=0
     int d=(5.0/9)*(f-32);                   // float/int=float but it is stored in int
     
-    float e=(5.0/9)*(g-32);                   // the most accurate one
+    float e=(5.0/9)*(g-32);                 // the most accurate one
 
     cout<<c<<endl;
     cout<<d<<endl;
