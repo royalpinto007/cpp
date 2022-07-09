a2ff9882e7f28d3fb80d8afd0b19d0d6e7af2d0c
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 17:11:12 +0530

    Update Pointers38.cpp

--- Pointers/Pointers38.cpp
+++ Pointers/Pointers38.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void Q(int z)
+void Q(int z)                                                   // 7 is passed from function P
 {
-    z += z;
-    cout << z << " ";
+    z += z;                                                     
+    cout << z << " ";                                           // 14                                      
 }
 
-void P(int *y)
+void P(int *y)                                                  // 5 is passed from main function
 {
-    int x = *y + 2;
+    int x = *y + 2;                                             // becomes 7 here
     Q(x);
-    *y = x - 1;
-    cout << x << " ";
+    *y = x - 1;                                                 // 7-1
+    cout << x << " ";                                           // 7
 }
 
 int main()
 {
     int x = 5;
     P(&x);
-    cout << x;
+    cout << x;                                                  // 6 (y is returned from function P) [Line 14]
     return 0;
 }
