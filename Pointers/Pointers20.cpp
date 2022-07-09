f92d475386e82e7b04fea4e2fd68880d28c31908
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 14:14:40 +0530

    Update Pointers20.cpp

--- Pointers/Pointers20.cpp
+++ Pointers/Pointers20.cpp
@@ -6,5 +6,5 @@ int main()
     char ch = 'a';
     char *ptr = &ch;
     ch++;
-    cout << *ptr << endl;
+    cout << *ptr << endl;                       // b (small a ascii- 97. ptr points to ch. ch is incremented and is now 98 (b))
 }
