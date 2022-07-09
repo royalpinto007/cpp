10f381b5dd891809ab883c62b7b2741c1a12e91c
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 14:32:11 +0530

    Update Pointers23.cpp

--- Pointers/Pointers23.cpp
+++ Pointers/Pointers23.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main()
 {
     char s[] = "hello";
-    char *p = s;
-    cout << s[0] << " " << p[0];
+    char *p = s;                                // For characters, we can directly initialize with the value (no need to use &)
+    cout << s[0] << " " << p[0];                // h h
 }
