2385a701fa5f4dacaef54450fca9c88949ea486e
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 16:56:32 +0530

    Update Pointers34.cpp

--- Pointers/Pointers34.cpp
+++ Pointers/Pointers34.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     char arr[20];
     int i;
     for (i = 0; i < 10; i++)
     {
-        *(arr + i) = 65 + i;
+        *(arr + i) = 65 + i;                    // ASCII of 65 is A
     }
     *(arr + i) = '\0';
-    cout << arr;
+    cout << arr;                                // ABCDEFGHIJ 
     return 0;
 }
