eedd42340ac2d9b6d753ccabcf9782b988c3ca95
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 11:47:51 +0530

    Update Pointers10.cpp

--- Pointers/Pointers10.cpp
+++ Pointers/Pointers10.cpp
@@ -6,5 +6,5 @@ int main()
     int a = 7;
     int *c = &a;
     c = c + 1;
-    cout << a << "  " << *c << endl;
+    cout << a << "  " << c << " " << *c << endl;            // a remains same= 7, c is the address of next storage, *c gives a garbage value of next storage
 }
