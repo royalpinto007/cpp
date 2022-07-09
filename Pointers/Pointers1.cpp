a47d6eb4a048ff4b08d078285ee0c907e0a93334
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 07:49:23 +0530

    Update Pointers1.cpp

--- Pointers/Pointers1.cpp
+++ Pointers/Pointers1.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-
     int a = 7;
     int b = 17;
     int *c = &b;
     *c = 7;
-    cout << a <<" "<< b << endl;
+    cout << a <<" "<< b << endl;                // 7 7
+    cout << c <<" "<< *c;                       // 0x126263 10
 }
