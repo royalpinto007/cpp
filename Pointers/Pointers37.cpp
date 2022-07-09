a71581d1755aef4950e9c9e86e496114a7541216
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 17:03:24 +0530

    Update Pointers37.cpp

--- Pointers/Pointers37.cpp
+++ Pointers/Pointers37.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     char st[] = "ABCD";
     for (int i = 0; st[i] != '\0'; i++)
     {
-        cout << st[i] << *(st) + i << *(i + st) << i[st];
+        cout << st[i] << *(st) + i << *(i + st) << i[st];                           //A65AAB66BBC67CCD68DD
     }
     return 0;
 }
+
+// It simply means- st[i] = *(i + st) = i[st];  
