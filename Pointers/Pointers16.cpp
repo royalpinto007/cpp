00d69788dfacb7dac905af905a4b963f09fca521
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 12:09:44 +0530

    Update Pointers16.cpp

--- Pointers/Pointers16.cpp
+++ Pointers/Pointers16.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
+
 // Assume that address of 0th index of array ‘a’ is : 200. What is the output -
 int main()
 {
     int a[3] = {1, 2, 3};
-    cout << *(a + 2);
+    cout << *(a + 2);                       // 3
 }
