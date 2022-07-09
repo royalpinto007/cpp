0fac6e492399511fdd4579fbf19617b9c08d2303
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 17:01:18 +0530

    Update Pointers36.cpp

--- Pointers/Pointers36.cpp
+++ Pointers/Pointers36.cpp
@@ -6,7 +6,7 @@ int main()
     float arr[5] = {12.5, 10.0, 13.5, 90.5, 0.5};
     float *ptr1 = &arr[0];
     float *ptr2 = ptr1 + 3;
-    cout << *ptr2 << " ";
-    cout << ptr2 - ptr1;
+    cout << *ptr2 << " ";                                           // 90.5
+    cout << ptr2 - ptr1;                                            // 3    (ptr2 = ptr+3)...so ptr2-ptr1=3
     return 0;
 }
