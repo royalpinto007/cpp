2001c7437bc79c4d1fc0d6af7262cbe8f4435689
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 16:52:00 +0530

    Update Pointers32.cpp

--- Pointers/Pointers32.cpp
+++ Pointers/Pointers32.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int numbers[5];
     int *p;
     p = numbers;
     *p = 10;
-    p = &numbers[2];
+    p = &numbers[2];                                        // this is similar to line 7 or 15
     *p = 20;
     p--;
     *p = 30;
-    p = numbers + 3;
+    p = numbers + 3;                                        // this is again similar to line 7 or 15
     *p = 40;
     p = numbers;
-    *(p + 4) = 50;
+    *(p + 4) = 50;                                          // another way of implementation
+    
     for (int n = 0; n < 5; n++)
     {
-        cout << numbers[n] << ",";
+        cout << numbers[n] << " ";                          // 10 30 20 40 50
     }
     return 0;
 }
