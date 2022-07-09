504cba0f80c16a600353fba3524f98b0c30c5347
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 16:53:23 +0530

    Update Pointers33.cpp

--- Pointers/Pointers33.cpp
+++ Pointers/Pointers33.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     char *ptr;
     char Str[] = "abcdefg";
     ptr = Str;
     ptr += 5;
-    cout << ptr;
+    cout << ptr;                                    // fg {Don't forget what happens with characters :)}
     return 0;
 }
