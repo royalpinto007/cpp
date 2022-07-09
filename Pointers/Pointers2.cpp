be531b7ead514c43789fe855c1fb0743ef5550d3
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 08:00:05 +0530

    Added Comments

--- Pointers/Pointers2.cpp
+++ Pointers/Pointers2.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-
     int a = 50;
     int *ptr = &a;
     int *q = ptr;
     (*q)++;
-    cout << a << endl;
+    cout << a <<" "<< ptr <<" "<< *ptr << endl;         // 51 0x61ff04 51
+    cout << q <<" "<< *q << endl;                       // 0x61ff04 51
 }
