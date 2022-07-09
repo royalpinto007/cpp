e00c7e50e6cdf1a2b4e4502ba012dad560848a62
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-09 12:09:14 +0530

    Update Pointers15.cpp

--- Pointers/Pointers15.cpp
+++ Pointers/Pointers15.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
+// Assume that address of 0th index of array ‘a’ is : 200.
 int main()
 {
     int a[6] = {1, 2, 3};
-    cout << a << " " << &a;
+    cout << a << " " << &a;                 // 200 200
 }
