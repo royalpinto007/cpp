6024d11c5304ad6b8f702210416e95c9bea5e785
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-10 12:22:12 +0530

    Create 4.cpp

---
+++ Dynamic Allocation/Notes/4.cpp
@@ -0,0 +1,10 @@
+// What is the correct syntax of declaring and defining a reference?
+  
+// This problem has only one correct answer
+
+// a) int a = 10; int &b; b = a;
+// b) int a = 10; int &b = a; 
+// c) int a = 10; int b = &a;
+// d) int a = 10; int &b = &a;
+
+// Answer- b
