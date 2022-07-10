a5990e94bf1d40e02f14333b2bdfd1af1ba4d8dd
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-10 14:37:00 +0530

    Update 5.cpp

--- Dynamic Allocation/Notes/5.cpp
+++ Dynamic Allocation/Notes/5.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main()
 {
 
-    int *p = new int;
+    int *p = new int;                       // dynamic allocation
     delete p;
 
     p = new int;
-    delete p;
+    delete p;                               // single element deletion (p delete nahi hua h, p jis integer ko point kr rha tha vo delete hua h)
 
     p = new int[100];
-    delete[] p;
+    delete[] p;                             // array deletion
 
     /*
     while (true) {
