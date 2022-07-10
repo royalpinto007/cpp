802b4e73b2a478a8acfad129b68d23ee5ca0526d
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-10 14:50:12 +0530

    2D Arrays

---
+++ Dynamic Allocation/Notes/7.cpp
@@ -0,0 +1,27 @@
+//Dynamic Allocation of 2D Arrays
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    int **p = new int *[m];
+  
+    // to input
+    for (int i = 0; i < m; i++)
+    {
+        p[i] = new int[i + 1];
+        for (int j = 0; j < n; j++)
+        {
+            cin >> p[i][j];
+        }
+    }
+    
+    // deleting the array
+    for (int i = 0; i < m; i++)
+    {
+        delete[] p[i];
+    }
+    delete[] p;
+}
