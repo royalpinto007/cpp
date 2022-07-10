6aa7a4c37217a92bc264224333f7edcd8ddcd8f4
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-10 15:00:22 +0530

    Update and rename DynamicAllocation6.cpp to DynamicAllocation5.cpp

--- Dynamic Allocation/DynamicAllocation5.cpp
+++ Dynamic Allocation/DynamicAllocation5.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int x = 1;
+int x = 1;                      // Global Variable
 
 void print()
 {
     int x = 2;
     {
         int x = 3;
-        cout << x << endl;
+        cout << x << endl;      // 3
     }
 }
+
 int main()
 {
     print();
