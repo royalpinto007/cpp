9c42dbcedaaad5718280ef14b106875983a08a25
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-11 16:04:43 +0530

    Update DynamicAllocation11.cpp

--- Dynamic Allocation/DynamicAllocation11.cpp
+++ Dynamic Allocation/DynamicAllocation11.cpp
@@ -6,6 +6,8 @@ int main()
     int p = 5;
     int const *q = &p;
     p++;
-    cout << p << endl;
+    cout << p << endl;                  // 6
     return 0;
 }
+
+// Value of p can be increased or decreased as it is not a const value.
