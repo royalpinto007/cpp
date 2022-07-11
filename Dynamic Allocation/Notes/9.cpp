7c91fa83890d8f9175bc5c35f16a4210aec506fb
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-11 15:19:20 +0530

    Inline and Default Arguments

--- Dynamic Allocation/Notes/9.cpp
+++ Dynamic Allocation/Notes/9.cpp
@@ -22,21 +22,24 @@ int main()
 }
 
 // default arguments
-int sum(int a[], int size, int si = 0) {
-	int ans = 0;
-	for (int i = si; i < size; i++) {
-		ans += a[i];
-	}
-	return ans;
+int sum(int a[], int size, int si = 0)
+{
+    int ans = 0;
+    for (int i = si; i < size; i++)
+    {
+        ans += a[i];
+    }
+    return ans;
 }
 
-int sum2(int a, int b = 0, int c = 0, int d = 0) {
-	return a + b + c + d;
+int sum2(int a, int b = 0, int c = 0, int d = 0)
+{
+    return a + b + c + d;
 }
 
-int main() {
-	 int a[20];
-	 // input code for a
-
-	 cout << sum(a, 20) << endl;
+int main()
+{
+    int a[20];
+    // input code for a
+    cout << sum(a, 20) << endl;
 }
