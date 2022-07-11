7e8f916586988ab2c74d6c5a77c3869d5f237b25
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-11 16:16:18 +0530

    Create DynamicAllocation14.cpp

---
+++ Dynamic Allocation/DynamicAllocation14.cpp
@@ -0,0 +1,16 @@
+// Which statement(s) will give an error for the following code?
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int p = 5;
+    int const *q = &p;
+}
+
+// This problem may have one or more correct answers
+// (a) p++; 
+// (b) q++
+// (c) (*q)++; 
+
+// Answer- (a) & (c)
