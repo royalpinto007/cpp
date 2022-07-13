979d89e02516fae2a930ce99727668cce0f5a200
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-13 18:14:00 +0530

    Update and rename datatypes2.cpp to Datatypes 2.cpp

--- Datatypes/Datatypes 2.cpp
+++ Datatypes/Datatypes 2.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-
+int main()
+{
     double a=6/4;                           //int/int=int again 6/4 becomes 1
     int b=6/4;
     double c=a+b;
