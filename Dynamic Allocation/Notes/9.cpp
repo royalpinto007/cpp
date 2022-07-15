8bb0e877f702789a497bfb5efc170425507ecc78
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-15 19:09:54 +0530

    Update 9.cpp

--- Dynamic Allocation/Notes/9.cpp
+++ Dynamic Allocation/Notes/9.cpp
@@ -2,9 +2,17 @@
 using namespace std;
 
 // inline arguments
-inline int max(int a, int b)
-{
-    return (a > b) ? a : b;
+inline int max(int a, int b)            // copies the function once to line 23 and again to 29, it increases readability of the code
+{   
+    // if(a>b)
+    // {
+    //  return a;
+    // }
+    // else
+    // {
+    //  return b;
+    // }
+    return (a > b) ? a : b;             // Line(7-14) is equal to this, known as tertiary operator
 }
 
 int main()
@@ -12,7 +20,7 @@ int main()
     int a, b;
     cin >> a >> b;
 
-    int c = max(a, b);
+    int c = max(a, b);                  
 
     int x, y;
     x = 12;
@@ -22,7 +30,7 @@ int main()
 }
 
 // default arguments
-int sum(int a[], int size, int si = 0)
+int sum(int a[], int size, int si = 0)   // rightmost element should also be declared first in order to have
 {
     int ans = 0;
     for (int i = si; i < size; i++)
@@ -43,3 +51,16 @@ int main()
     // input code for a
     cout << sum(a, 20) << endl;
 }
+
+
+//
+// Inline functions are useful when?
+    
+// (a) Function is large with many nested loops
+// (b) Function has many static variables
+// (c) Function is small and we want to avoid function call overhead. 
+// (d) None of the above
+
+// Answer- (c)
+// Inline functions are used to reduce the function call overhead. 
+// Inline function is a function that is expanded in line when it is called.
