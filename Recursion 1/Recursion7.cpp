a0b6b64ed1a3db4155168258e75adb0ca881bd0f
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-16 10:37:22 +0530

    Create Recursion7.cpp

---
+++ Recursion 1/Recursion7.cpp
@@ -0,0 +1,42 @@
+// Sum of Array
+
+/*
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+
+Sample Input 1 :
+3
+9 8 9
+  
+Sample Output 1 :
+26
+*/
+
+int sum(int input[], int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    return input[0] + sum(input + 1, n - 1);
+}
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *input = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input[i];
+    }
+
+    cout << sum(input, n) << endl;
+}
+*/
