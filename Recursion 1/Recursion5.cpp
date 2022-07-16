ee65d597ef864bb76563a10b53b07dc150fd1579
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-16 08:33:15 +0530

    Update Recursion5.cpp

--- Recursion 1/Recursion5.cpp
+++ Recursion 1/Recursion5.cpp
@@ -1,23 +1,28 @@
-// What will be the output ?
-
 #include <iostream>
 using namespace std;
 
-void print(int n){
-    if(n < 0){
+void print(int n)
+{
+    if(n < 0)
+    {
         return;
     }
     cout << n << " ";
     print(n - 2);
 }
 
-int main() {
+int main() 
+{
     int num = 5;
     print(num);
 }
 
+// What will be the output ?
+
 // This problem has only one correct answer
 // (a) time error
-//5 4 3 2 1
-// 5 3 1 
-// None of these
+// (b) 5 4 3 2 1
+// (c) 5 3 1 
+// (d) None of these
+
+// Answer- 3
