276bc63b4fb5b973b057061b911a13e58a182dd3
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-20 19:49:51 +0530

    Update Fibonacci Number1.cpp

--- Functions/Fibonacci Number1.cpp
+++ Functions/Fibonacci Number1.cpp
@@ -1,6 +1,5 @@
 bool checkMember(int n)
 {
-
     int a = 0;
     int b = 1;
     if (n == a || n == b)
@@ -22,7 +21,6 @@ using namespace std;
 
 int main()
 {
-
     int n;
     cin >> n;
     if (checkMember(n))
