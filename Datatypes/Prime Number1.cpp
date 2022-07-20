53fbdf3131b14decfb22aecd4d25bafb45097531
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-20 19:47:04 +0530

    Update and rename primenumber1.cpp to Prime Number1.cpp

---
+++ Datatypes/Prime Number1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    bool dividend = false;
+    for (int d = 2; d < n; d++)
+    {
+        if (n % d == 0)
+        {
+            dividend = true;
+        }
+    }
+    if (dividend)
+    {
+        cout << "Not Prime";
+    }
+    else
+    {
+        cout << "Prime";
+    }
+}
