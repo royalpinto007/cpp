9a5187fc6fec8d467cda5fcb66e8aadd8f756411
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-20 19:48:05 +0530

    Update Factorial.cpp

--- Functions/Factorial.cpp
+++ Functions/Factorial.cpp
@@ -5,24 +5,27 @@ nCr Calculation
 #include<iostream>
 using namespace std;
 
-int factorial(int n){
-    int ans=1;
-    int i=1;
-    while(i<=n){
-        ans=ans*i;
+int factorial(int n)
+{
+    int ans = 1;
+    int i = 1;
+    while (i <= n)
+    {
+        ans = ans * i;
         i++;
     }
     return ans;
 }
 
-int main(){
-    int n,r;
-    cin>>n>>r;
-    
-    int fact_n=factorial(n);
-    int fact_r=factorial(r);
-    int fact_n_r=factorial(n-r);
+int main()
+{
+    int n, r;
+    cin >> n >> r;
 
-    int ans=(fact_n)/((fact_n_r)*(fact_r));
-    cout<<ans<<endl;
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_n_r = factorial(n - r);
+
+    int ans = (fact_n) / ((fact_n_r) * (fact_r));
+    cout << ans << endl;
 }
