7842108522695667bea9dd2493d72211f8aed9e2
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-23 15:04:59 +0530

    Create Replace Character.cpp

---
+++ Recursion 2/Notes/Replace Character.cpp
@@ -0,0 +1,50 @@
+/*
+Sample Input :
+abacd
+a x
+
+Sample Output :
+xbxcd
+*/
+void replaceCharacter(char input[], char c1, char c2) 
+{
+    if (input[0]=='\0')
+    {
+        return;
+    }
+    
+    input[0]=(input[0]==c1)?c2:input[0];
+    replaceCharacter(input+1,c1,c2);
+       
+}
+
+/* 
+void replaceCharacter(char input[], char c1, char c2) 
+{ 
+	if(input[0] == '\0') 
+	{ 
+		return; 	
+    } 
+
+	if(input[0] == c1) 
+	{ 
+		input[0] = c2; 
+	} 
+	replaceCharacter(input + 1, c1, c2); 
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char input[1000000];
+    char c1, c2;
+    cin >> input;
+    cin >> c1 >> c2;
+    replaceCharacter(input, c1, c2);
+    cout << input << endl;
+}
+*/
