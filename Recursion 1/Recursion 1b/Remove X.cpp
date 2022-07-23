57be71a0faf3fdfa81b44338f691b4d539a720e7
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-23 09:40:52 +0530

    Create Remove X.cpp

---
+++ Recursion 1/Recursion 1b/Remove X.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void removeX(char input[]) 
+{
+    if(input[0]=='\0')
+        return;
+    if(input[0]!='x')
+    {
+        removeX(input+1);
+    }
+    else
+    {
+        int i=1;
+        for(;input[i]!='\0';i++)
+        {
+            input[i-1]=input[i];
+        }
+        input[i-1]=input[i];
+        removeX(input);
+    }
+}
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char input[100];
+    cin.getline(input, 100);
+    removeX(input);
+    cout << input << endl;
+}
