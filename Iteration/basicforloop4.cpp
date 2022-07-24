524161128083f7aa305f95142291258acb5316ea
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-24 23:42:51 +0530

    Update basicforloop4.cpp

--- Iteration/basicforloop4.cpp
+++ Iteration/basicforloop4.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-for(int i=0; i<2; i++){
-    for(int j=0; j<2; j++){
-        if(j==1)
+int main()
+{
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<2; j++)
+        {
+            if(j==1)
             break;
             cout<<j<<" ";
+        }
     }
-}
 }
