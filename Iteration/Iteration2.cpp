2112b28d728464e7d616757873c0dd0b50adba72
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-26 13:54:27 +0530

    Update and rename iteration2.cpp to Iteration2.cpp

--- Iteration/Iteration2.cpp
+++ Iteration/Iteration2.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int i=1;
-    while(i<5){
-        if (i==3){
+    while(i<5)
+    {
+        if (i==3)
+        {
             break;
         }
         cout<<i<<" ";
