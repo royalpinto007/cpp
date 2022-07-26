8215d03222ec082f30eeb18f31a2457f462d8d01
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-26 14:14:10 +0530

    Update and rename iteration3.cpp to Iteration3.cpp

---
+++ Iteration/Iteration3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+        if (i==4)
+        {
+            continue;                            // till 3 it will print and then will become an infinite loop 
+        }
+    cout<<i<<" ";
+    i++;
+    }
+}
