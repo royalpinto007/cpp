5e771fb608bddb135e381264a986c1920a92f042
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-26 14:14:36 +0530

    Update and rename iteration4.cpp to Iteration4.cpp

--- Iteration/Iteration4.cpp
+++ Iteration/Iteration4.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int i=1;
-    while(i<3){
+    while(i<3)
+    {
         int j=1;
-        while(j<5){
-            if(j==3){
+        while(j<5)
+        {
+            if(j==3)
+            {
                 break;
             }
             cout<<j<<" ";
