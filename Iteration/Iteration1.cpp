d8b9c1903f05b4c1c46ef31a476936ae059067be
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-26 13:53:18 +0530

    Update and rename iteration1.cpp to Iteration1.cpp

--- Iteration/Iteration1.cpp
+++ Iteration/Iteration1.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int i=1;
-    while(i<10){
-        if(i==7){
+    while(i<10)
+    {
+        if(i==7)
+        {
             i++;
             continue;
         }
@@ -12,8 +15,10 @@ int main(){
         i++;
     }
     cout<<endl;
-    for(int j=1; j<10; j++){
-        if(j==7){
+    for(int j=1; j<10; j++)
+    {
+        if(j==7)
+        {
             break;
         }
         cout<<j<<" ";
