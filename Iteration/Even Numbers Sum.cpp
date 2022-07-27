4892a141ce81029bf8d3962a9d6f09c2f3625f56
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-27 08:52:14 +0530

    Update and rename evennumberssum.cpp to Even Numbers Sum.cpp

--- Iteration/Even Numbers Sum.cpp
+++ Iteration/Even Numbers Sum.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-
+int main()
+{
     int sum=0, n, i=2;
     cin>>n;
 
-    while(i<=n){
-        if(i%2==0){
+    while(i <= n)
+    {
+        if(i % 2 == 0)
+        {
             sum=sum+i;
 
         }
         i++;
-
     }
     cout<<sum;
 }
