259ffa305932a89e92a371e87a29124113dc88be
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-29 00:05:22 +0530

    Update and rename sumofoddevendigits.cpp to Sum of Odd-Even Digits.cpp

--- Iteration/Sum of Odd-Even Digits.cpp
+++ Iteration/Sum of Odd-Even Digits.cpp
@@ -1,25 +1,28 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
-
+    
     int number;
     int sumo=0;
     int sume=0;
 
-    while(n>0){
+    while(n>0)
+    {
         number=n%10;                    //to make the number smaller, in order to save time
 
-        if (number%2==0){
+        if (number%2==0)
+        {
             sume=sume+number;
         }
-        else{
+        else
+        {
             sumo=sumo+number;
         }
         n=n/10;                        //for next digit
-
     }
     cout<<sume<<" "<<sumo;
 }
