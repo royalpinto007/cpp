43ab5135134d4f10a577de0ad2d163ce2881b64c
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-29 00:06:08 +0530

    Update and rename sumorproduct.cpp to Sum or Product.cpp

--- Iteration/Sum or Product.cpp
+++ Iteration/Sum or Product.cpp
@@ -1,4 +1,5 @@
-/* Write a program that asks the user for a number N and a choice C.
+/* 
+Write a program that asks the user for a number N and a choice C.
 And then give them the possibility to choose between computing the sum
 and computing the product of all integers in the range 1 to N (both inclusive).
 
@@ -6,13 +7,12 @@ and computing the product of all integers in the range 1 to N (both inclusive).
 2, then print the product
 Any other number, then print '-1' (without the quotes)
 
-Input=
+Input-
 10 
 1
 
 Output-
 55
-
 */
 
 #include <iostream>
@@ -20,10 +20,8 @@ using namespace std;
 
 int main()
 {
-
     int n;
     int r;
-
     cin >> n >> r;
 
     int sum = 0;
