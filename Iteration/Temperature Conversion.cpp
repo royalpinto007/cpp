0ac24e1ff2e7ea6074d8a3a75c2beb034d204c76
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-28 16:37:34 +0530

    Update and rename temperatureconversion.cpp to Temperature Conversion.cpp

--- Iteration/Temperature Conversion.cpp
+++ Iteration/Temperature Conversion.cpp
@@ -1,4 +1,5 @@
-/*Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), 
+/*
+Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), 
 you need to convert all Fahrenheit values from Start to End at the gap of W, 
 into their corresponding Celsius values and print the table.
 */
@@ -6,13 +7,14 @@ into their corresponding Celsius values and print the table.
 #include<iostream>
 using namespace std;
 
-int main(){
-
+int main()
+{
     int s,e,w;
     cin>>s>>e>>w;
 
     int c;
-    for(int i=s; i<+e; i=i+w){
+    for(int i=s; i<+e; i=i+w)
+    {
         c=(i-32)*(5.0/9);
         cout<<i<<" "<<c<<endl;
     }
