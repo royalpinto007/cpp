3179b20ea0935e73894adc572d634d95f617270c
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-28 16:34:28 +0530

    Update and rename print1ton.cpp to Print 1toN.cpp

--- Iteration/Print 1toN.cpp
+++ Iteration/Print 1toN.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-
+int main()
+{
     int n;
     cin>>n;
-
     int i=1;
-    while(i<=n){
+   
+    while(i<=n)
+    {
         cout<<i<<" ";
         i++;
     }
