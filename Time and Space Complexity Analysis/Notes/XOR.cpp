6e30b6b7ec2a7bb3f7f280fbd5d07d9d7c136410
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-29 18:56:15 +0530

    Create XOR.cpp

---
+++ Time and Space Complexity Analysis/Notes/XOR.cpp
@@ -0,0 +1,7 @@
+// Remember these two properties of XOR operator :
+
+// (1) If you take xor of a number with 0 ( zero ) , it would return the same number again.
+// Means , n ^ 0 = n
+
+// (2) If you take xor of a number with itself , it would return 0 ( zero ).
+// Means , n ^ n = 0
