b7f686542fb516a09b7580910c707bdff82db15d
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-30 10:52:06 +0530

    Create 4.cpp

---
+++ OOPS 1/Notes/4.cpp
@@ -0,0 +1,29 @@
+// 1) What will be the output of the following program ?
+  
+// #include <iostream>
+// using namespace std;
+
+// class Shape{
+//     public : 
+//     int height;
+//     int width;
+// };
+
+// int main() {
+//     Shape *s = new Shape();
+//     s -> height = 1;
+//     cout << s -> height;
+// }
+
+// Output- 1
+
+
+// 2) Which statement is not true in C++ language ?
+
+// This problem has only one correct answer
+// (a) A public member of a class can be accessed outside the class.
+// (b) A private member of a class cannot be accessed by the functions of the same class. 
+// (c) A private member of a class cannot be accessed outside the same class.
+// (d) None of the above
+
+// Answer- (b)
