ba1f250253ec3d00894402604b96d690c6d6bc8d
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-30 10:48:38 +0530

    Create 2.cpp

---
+++ OOPS 1/Notes/2.cpp
@@ -0,0 +1,28 @@
+// 1) Which of the following is/are a valid statement(s) to create an object of class Bird in C++ ?
+  
+// This problem may have one or more correct answers
+// (a) Bird b = new Bird;
+// (b) Bird *b = new Bird; 
+// (c) Bird b; 
+// (d) new Bird b;
+
+// Answer- (b) & (c)
+
+
+// 2) Figure out the correct statement
+// class Car{
+//     public : 
+//     int price;
+// };
+
+// int main() {
+//     Car c; 
+//     //Figure out the correct statement to set the price of the car object referred to by c to “10000”
+// }
+
+// This problem has only one correct answer
+// (a) c->price = 10000
+// (b) Car.price = 10000
+// (c) c.price = 10000; 
+
+// Answer- (c)
