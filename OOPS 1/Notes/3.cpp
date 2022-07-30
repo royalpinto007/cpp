1899fcc5922f9d807d5479a5ee05b5d50525768e
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-30 10:50:33 +0530

    Create 3.cpp

---
+++ OOPS 1/Notes/3.cpp
@@ -0,0 +1,23 @@
+// What is the output -
+  
+// #include <iostream>
+// using namespace std;
+
+// class Student {
+//     int rollNumber;
+// };
+
+// int main() {
+//     Student s;
+//     cout << s.rollNumber;
+// }
+
+// This problem has only one correct answer
+
+// (a) GarbageValue
+// (b) 0
+// (c) Compilation Error 
+// (d) None
+
+// Answer- (c)
+//   Because objects should always be initialised with some value
