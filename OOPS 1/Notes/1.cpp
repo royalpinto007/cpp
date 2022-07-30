be17c814d88de882e7a8470f65e95738c999ec96
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-07-30 10:40:00 +0530

    Create 1.cpp

---
+++ OOPS 1/Notes/1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Student 
+{
+	public :
+	int rollNumber;
+	
+	private :
+	int age;
+};
+
+int main() 
+{
+	// Create objects statically
+	Student s1;
+	Student s2;
+	Student s3, s4, s5;
+
+  // adding values, remember we must always initialise the objects of the class with some value
+	s1.age = 24;
+	s1.rollNumber = 101;
+
+	cout << s1.age << endl;
+	cout << s1.rollNumber << endl;
+
+	s2.age = 30;
+
+	// Create objects dynamicaaly
+	Student *s6 = new Student;
+	(*s6).age = 23;
+	(*s6).rollNumber = 104;
+
+	s6 -> age = 23;
+	s6 -> rollNumber = 104;
+}
