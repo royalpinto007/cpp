b252d63e8919e8edc6524657e45b3106d4c43fdf
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-01 12:41:25 +0530

    Update and rename pattern9.cpp to Pattern9.cpp

--- Patterns/Pattern9.cpp
+++ Patterns/Pattern9.cpp
@@ -1,27 +1,29 @@
 /* 
-1234
-1234
-1234
-1234
+    1234
+    1234
+    1234
+    1234
 */
 
 
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
 
     int i=1;
-    while(i<=n){
+    while(i<=n)
+    {
         int j=1;
-        while(j<=n){
+        while(j<=n)
+        {
             cout<<j;
             j++;
         }
     cout<<endl;
     i++;
     }
-
 }
