2a3a0542368136ca86461dcdc50c926543ea1106
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-01 12:40:49 +0530

    Update and rename pattern8.cpp to Pattern8.cpp

--- Patterns/Pattern8.cpp
+++ Patterns/Pattern8.cpp
@@ -1,26 +1,28 @@
 /* 
-1111
-2222
-3333
-4444
+    1111
+    2222
+    3333
+    4444
 */
 
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
 
     int i=1;
-    while(i<=n){
+    while(i<=n)
+    {
         int j=1;
-        while(j<=n){
+        while(j<=n)
+        {
             cout<<i;
             j++;
         }
     cout<<endl;
     i++;
     }
-
 }
