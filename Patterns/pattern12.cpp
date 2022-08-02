ea9ec15267c97586b0e3836c8786e39ef94f7b2f
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-02 13:28:34 +0530

    Update pattern12.cpp

--- Patterns/pattern12.cpp
+++ Patterns/pattern12.cpp
@@ -1,28 +1,28 @@
 /*
-
-1
-22
-333
-4444
-
+    1
+    22
+    333
+    4444
 */
 
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
 
     int i=1;
-    while(i<=n){
+    while(i<=n)
+    {
         int j=1;
-        while(j<=i){
+        while(j<=i)
+        {
             cout<<i;
             j++;
         }
     cout<<endl;
     i++;
     }
-
 }
