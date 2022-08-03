f83c74a185978f3559d3953932fbf4192b360d4c
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-03 12:03:50 +0530

    Create 1.cpp

---
+++ Linked List 1/Notes/1.cpp
@@ -0,0 +1,30 @@
+class Node 
+{
+	public :
+	int data;
+	Node *next;
+
+	Node(int data) 
+  {
+		this -> data = data;
+		next = NULL;
+	}
+};
+
+/*
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+int main() 
+{
+	// Statically
+	Node n1(1);
+	Node *head = &n1;
+
+	Node n2(2);
+	n1.next = &n2;
+
+	cout << n1.data << " " << n2.data << endl;
+}
+*/
