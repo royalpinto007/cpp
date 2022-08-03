3c4a58d385047a2f763724b02dfba8b3abc3c122
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-03 09:42:33 +0530

    Update and rename pattern17.cpp to Pattern17.cpp

--- Patterns/Pattern17.cpp
+++ Patterns/Pattern17.cpp
@@ -1,19 +1,22 @@
 /*
-1111
-222
-33
-4
+    1111
+    222
+    33
+    4
 */
 
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n-i+1; j++){
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n-i+1; j++)
+        {
             cout<<i;
         }
         cout<<endl;
