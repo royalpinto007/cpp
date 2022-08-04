49dd29ebbe3e4733a8155c0f675fd26c041e5dc3
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-05 00:06:06 +0530

    Update and rename pattern23.cpp to Pattern23.cpp

--- Patterns/Pattern23.cpp
+++ Patterns/Pattern23.cpp
@@ -1,24 +1,25 @@
 /*
-
-ABCD
-BCDE
-CDEF
-DEFG
-
+    ABCD
+    BCDE
+    CDEF
+    DEFG
 */
 
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
 
     int i=1;
-    while(i<=n){
+    while(i<=n)
+    {
         int j=1;
         char startchar='A'+i-1;                             // assigning a variable startchar, to make our work easy!
-        while(j<=n){
+        while(j<=n)
+        {
             char ch=startchar+j-1;
             cout<<ch;
             j++;
