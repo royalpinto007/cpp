4d4f132619aa0b0775c25be77fab909f3ababce1
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-05 00:07:34 +0530

    Update and rename pattern25.cpp to Pattern25.cpp

--- Patterns/Pattern25.cpp
+++ Patterns/Pattern25.cpp
@@ -1,36 +1,35 @@
 /*
-
-   A
-  BB
- CCC
-DDDD
-
+      A
+     BB
+    CCC
+   DDDD
 */
 
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
 
     int i=1;
-    while(i<=n){
-        
+    while(i<=n)
+    {
         int spaces=1;
-        while(spaces<=n-i){
+        while(spaces<=n-i)
+        {
             cout<<" ";
             spaces++;
         }
 
         int j=1;
-        while(j<=i){
+        while(j<=i)
+        {
             char ch='A'+i-1;
             cout<<ch;
             j++;
-        }
-
-        
+        }      
         i++;
         cout<<endl;
     }
