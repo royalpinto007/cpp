d689f636afbea4d3b97f7b4ca0a02c2524078b18
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-05 00:06:35 +0530

    Update and rename pattern24.cpp to Pattern24.cpp

--- Patterns/Pattern24.cpp
+++ Patterns/Pattern24.cpp
@@ -1,23 +1,24 @@
 /*
-
-A
-BB
-CCC
-DDDD
-
+    A
+    BB
+    CCC
+    DDDD
 */
 
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
 
     int i=1;
-    while(i<=n){
+    while(i<=n)
+    {
         int j=1;
-        while(j<=i){
+        while(j<=i)
+        {
             char ch='A'+i-1;
             cout<<ch;
             j++;
