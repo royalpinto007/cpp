bd58c16d683924e3503a8bd0e44349942cc0616d
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-05 18:38:25 +0530

    Create 3.cpp

---
+++ Linked List 1/MCQ's/3.cpp
@@ -0,0 +1,43 @@
+// What is the Output?
+
+// class Node
+// {
+//     public:
+//         int data;
+//         Node *next;
+
+//         Node(int data)
+//         {
+//             this -> data = data;
+//             this -> next = NULL;
+//         }
+// };
+
+
+// void print(Node *head)
+// {
+//     Node *temp = head;
+
+//     while(temp != NULL)
+//     {
+//         cout << temp -> data << " ";
+//         temp = temp -> next;
+//     }
+//     cout << endl;
+// }
+
+// int main()
+// {
+//     Node *node1 = new Node(10);
+//     Node *node2 = new Node(20);
+//     node2 -> next = node1;
+//     print(node2);
+// }
+
+// This problem has only one correct answer
+// (a) 10 20
+// (b) 20 10 
+// (c) Error
+// (d) None of these
+
+// Answer- (b)
