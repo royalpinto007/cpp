961e22e7868f846faa1f3934b5cd8bd52c1d1f55
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-05 18:34:06 +0530

    Create 1.cpp

---
+++ Linked List 1/MCQ's/1.cpp
@@ -0,0 +1,9 @@
+// Linked List are best suited:
+
+// This problem has only one correct answer
+// (a) for relatively permanent collections of data.
+// (b) for the size of the structure is constantly changing. 
+// (c) for random Access of elements
+// (d) None of these
+
+// Answer- (c)
