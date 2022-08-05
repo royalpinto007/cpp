4e8c5af2c46edd8cbcc865076281133741c84e0e
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-05 17:32:42 +0530

    Update and rename pattern27.cpp to Pattern27.cpp

--- Patterns/Pattern27.cpp
+++ Patterns/Pattern27.cpp
@@ -1,25 +1,26 @@
 /*
-
-E
-DE
-CDE
-BCDE
-ABCDE
-
+    E
+    DE
+    CDE
+    BCDE
+    ABCDE
 */
 
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
 
     int i=1;
-    while(i<=n){
+    while(i<=n)
+    {
         int j=1;
         char start='A'+n-i;
-        while(j<=i){
+        while(j<=i)
+        {
             char ch=start+j-1;
             cout<<ch;
             j++;
