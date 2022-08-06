1a93e6b1a085f7f1732fe6f1fd2fc77b141dd8a2
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-06 22:16:37 +0530

    Update and rename scope1.cpp to Scope1.cpp

---
+++ Scope/Scope1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    if (a>5)
+    {
+        int b=10;
+    }
+    cout<<b;  //doesnt work(code error) since b is inside the if condition and scope is limited to the brackets
+}
