eb4a98c538f2501dc653a2b2c02acd9334dc2184
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-06 08:00:39 +0530

    Update and rename print1ton1.cpp to Print 1toN(1).cpp

--- Iteration/Print 1toN(1).cpp
+++ Iteration/Print 1toN(1).cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
 
-void print1_to_n(int n){
-    for(int i=1; i<=n; i++){
+void print1_to_n(int n)
+{
+    for(int i=1; i<=n; i++)
+    {
         cout<<i<<endl;
     }
 }
 
-int main(){
+int main()
+{
     print1_to_n(10);
 }
