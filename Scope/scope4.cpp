cc7aa2049fb32834ed0986c2d4305858b87486fd
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-07 16:24:35 +0530

    Update scope4.cpp

--- Scope/scope4.cpp
+++ Scope/scope4.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    for(int i=0; i<3; i++){
+int main()
+{
+    for(int i=0; i<3; i++)
+    {
         cout<<i<<" ";
     }
-  //  cout<<i<<endl;                   i wasnt declared in this scope
+  // cout<<i<<endl;                   i wasn't declared in this scope
 }
