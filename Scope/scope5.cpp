bb824821b2eae10de297452b3c1edd06deb1c19b
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-07 16:25:42 +0530

    Update scope5.cpp

--- Scope/scope5.cpp
+++ Scope/scope5.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int a=10;
-    while(a>5){
+    while(a>5)
+    {
         int a=1;
         cout<<a<<" ";
-        a--;                        //infinite 1's will be printed
+        a--;                        // infinite 1's will be printed
     }
 }
