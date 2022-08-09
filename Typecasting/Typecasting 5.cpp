bd28cfaaaa7cf2f6db267fe21963553c5f151ff3
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-09 08:48:34 +0530

    Update and rename basictypecasting4.cpp to Typecasting 5.cpp

--- Typecasting/Typecasting 5.cpp
+++ Typecasting/Typecasting 5.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int i=10;
     char c='a';
+    
     int o=i+c;
     char d=i+c;
+    
     cout<<o<<endl;
     cout<<d;
 }
