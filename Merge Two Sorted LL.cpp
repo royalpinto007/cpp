108d9dfc5987ed859b2baa4f8e02095ca6060b54
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-14 19:08:28 +0530

    Create Merge Two Sorted LL.cpp

---
+++ Merge Two Sorted LL.cpp
@@ -0,0 +1,116 @@
+/*
+Sample Input 1 :
+1
+2 5 8 12 -1
+3 6 9 -1
+Sample Output 1 :
+2 3 5 6 8 9 12 
+*/
+
+Node* mergeTwoSortedLinkedLists(Node *head1, Node *head2) 
+{
+    Node *p=NULL,*q=NULL;
+    if(head1==NULL)	 return head2;
+    if(head2==NULL)  return head1;
+    if(head1->data<head2->data)
+    {
+        p=head1;
+        q=head1;
+        head1=head1->next;       
+    }
+    else if(head1->data>=head2->data)
+    {
+        p=head2;
+        q=head2;
+        head2=head2->next;  
+    }
+    while(head1&&head2)
+    {
+       if(head1->data<head2->data)
+       {
+           q->next=head1;
+           q=head1;
+           head1=head1->next;
+           q->next=NULL;
+       }
+    else if(head1->data>=head2->data)
+       {
+           q->next=head2;
+           q=head2;
+           head2=head2->next;
+           q->next=NULL;
+       }
+    }
+    if(head1)
+        q->next=head1;
+    if(head2)
+        q->next=head2;
+   
+    return p;
+}
+
+/*
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head1 = takeinput();
+		Node *head2 = takeinput();
+		Node *head3 = mergeTwoSortedLinkedLists(head1, head2);
+		print(head3);
+	}
+	return 0;
+}
+*/
