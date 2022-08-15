c8ccba3c932c6376821ab6a921433ee1fff1c573
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-15 13:19:56 +0530

    Create Find a Node in LL.cpp

---
+++ Find a Node in LL.cpp
@@ -0,0 +1,85 @@
+/*
+Sample Input 1 :
+2
+3 4 5 2 6 1 9 -1
+5
+10 20 30 40 50 60 70 -1
+6
+  
+Sample Output 1 :
+2
+-1
+*/
+
+int findNode(Node *head, int n)
+{
+    Node *temp= head;
+    int count=0;
+    if(temp == NULL)
+    {
+        return -1;
+    }      
+    while(temp != NULL && temp->data  != n )
+    {
+        count++;
+        temp= temp->next;
+    }
+    if(temp != NULL &&temp->data ==n)
+    {
+        return count;
+    }
+    return -1;
+}
+
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int val;
+		cin >> val;
+		cout << findNode(head, val) << endl;
+	}
+}
