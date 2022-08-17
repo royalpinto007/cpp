4eb9dc2223503735bfc8a8f18d0987f4b308046f
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-17 23:00:51 +0530

    Create Merge Sort.cpp

---
+++ Merge Sort.cpp
@@ -0,0 +1,151 @@
+int length(Node *head) {
+   
+    int count=0;
+    while(head)
+    {
+        count++;
+        head=head->next;
+    }
+    return count;
+}
+Node* mergeTwoLLs(Node *head1, Node *head2) {
+
+    Node *p=NULL,*q=NULL;
+    if(head1->data<head2->data)
+    {
+        p=head1;
+        q=head1;
+        head1=head1->next;
+        
+        
+    }
+    else if(head1->data>=head2->data)
+    {
+        p=head2;
+        q=head2;
+        head2=head2->next;
+       
+        
+    }
+    while(head1&&head2)
+    {
+       if(head1->data<head2->data)
+       {
+           q->next=head1;
+           q=head1;
+           head1=head1->next;
+           q->next=NULL;
+       }
+    else if(head1->data>=head2->data)
+       {
+           q->next=head2;
+           q=head2;
+           head2=head2->next;
+           q->next=NULL;
+       }
+    }
+    if(head1)
+        q->next=head1;
+    if(head2)
+        q->next=head2;
+    
+    
+    return p;
+}
+
+Node* mergeSort(Node *head) {
+    //write your code here
+   if(head==NULL|| head->next==NULL)
+        {return head;}
+     
+     //breaking node into two half  
+    /*Node *p=head;
+    for(int i=1;i<length(head)/2;i++)
+    {
+        p=p->next;
+    }
+    Node *head1=p->next;
+    p->next=NULL;*/
+    Node* slow=head;
+    Node* fast=head->next;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    Node* head1=slow->next;
+    slow->next=NULL;
+
+    head=mergeSort(head);
+    head1=mergeSort(head1);
+
+    return mergeTwoLLs(head,head1);
+    
+}
+
+/*
+#include <iostream>
+using namespace std;
+
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		head = mergeSort(head);
+		print(head);
+	}
+
+	return 0;
+}
+*/
