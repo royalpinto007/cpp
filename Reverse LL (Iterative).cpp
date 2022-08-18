456a6a348b76ad6f303eff53054ce1a6720e3daf
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-18 23:22:50 +0530

    Create Reverse LL (Iterative).cpp

---
+++ Reverse LL (Iterative).cpp
@@ -0,0 +1,70 @@
+Node *reverseLinkedList(Node *head)
+{
+    Node *p=head,*q=NULL,*r=NULL;
+    while(p)
+    {
+        r=q;
+        q=p;
+        p=p->next;
+        q->next=r;
+    }
+    
+    return q;
+}
+
+/*
+#include <iostream>
+using namespace std;
+
+
+class Node {
+   public:
+    int data;
+    Node *next;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node *takeinput() {
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1) {
+        Node *newnode = new Node(data);
+        if (head == NULL) {
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail->next = newnode;
+            tail = newnode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head) {
+    Node *temp = head;
+
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        Node *head = takeinput();
+        head = reverseLinkedList(head);
+        print(head);
+    }
+
+    return 0;
+}
