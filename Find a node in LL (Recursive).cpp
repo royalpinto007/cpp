afe5f4457fcf232a809ffbcf22b7d8a6afe4fff9
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-19 20:24:40 +0530

    Update Find a node in LL (Recursive).cpp

--- Find a node in LL (Recursive).cpp
+++ Find a node in LL (Recursive).cpp
@@ -12,3 +12,56 @@ int findNodeRec(Node *head, int n)
     else
         return x+1;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int val;
+		cin >> val;
+		cout << findNodeRec(head, val) << endl;
+	}
+}
+*/
