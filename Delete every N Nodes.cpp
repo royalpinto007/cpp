8af5576e85ea3c3a87fb1e40eadb48fd10c29c48
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-20 22:30:08 +0530

    Update Delete every N Nodes.cpp

--- Delete every N Nodes.cpp
+++ Delete every N Nodes.cpp
@@ -42,3 +42,69 @@ Node* skipMdeleteN(Node *head, int M, int N)
     t1->next=m;
     return head;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int m, n;
+		cin >> m >> n;
+		head = skipMdeleteN(head, m, n);
+		print(head);
+	}
+	return 0;
+}
+*/
