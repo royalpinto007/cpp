129247c9f5b4d7517daabfb93ac8a180e908e86b
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-22 21:20:22 +0530

    Update kReverse.cpp

--- Linked List 2/Assignment/kReverse.cpp
+++ Linked List 2/Assignment/kReverse.cpp
@@ -66,3 +66,72 @@ Node *kReverse(Node *head, int k) {
     } 
     return prev; 
 }
+
+
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		int k;
+		cin >> k;
+		head = kReverse(head, k);
+		print(head);
+	}
+	
+	return 0;
+}
