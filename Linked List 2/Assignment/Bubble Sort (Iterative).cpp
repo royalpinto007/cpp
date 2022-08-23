ab333fe23e06bad230e4bf4548b6d65cc14bac21
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-23 20:27:32 +0530

    Update Bubble Sort (Iterative).cpp

--- Linked List 2/Assignment/Bubble Sort (Iterative).cpp
+++ Linked List 2/Assignment/Bubble Sort (Iterative).cpp
@@ -55,3 +55,58 @@ Node *bubbleSort(Node *head)
   
   return head;
 }
+
+/*
+#include <iostream>
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+int main()
+{
+	Node *head = takeinput();
+	head = bubbleSort(head);
+	print(head);
+}
