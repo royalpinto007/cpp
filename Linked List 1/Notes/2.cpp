a20d8675864d619e76716649dc1ace5d8838caa7
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-24 09:52:27 +0530

    Update 2.cpp

--- Linked List 1/Notes/2.cpp
+++ Linked List 1/Notes/2.cpp
@@ -5,15 +5,15 @@ class Node
 	Node *next;
 
 	Node(int data) 
-  {
+ 	{
 		this -> data = data;
 		next = NULL;
 	}
 };
 
+/*
 #include <iostream>
 using namespace std;
-#include "Node.cpp"
 
 void print(Node *head) 
 {
@@ -33,30 +33,30 @@ void print(Node *head)
 		temp = temp -> next;
 	  }
   */
-}
+// }
 
-int main() 
-{
-	// Statically
-	Node n1(1);
-	Node *head = &n1;
+// int main() 
+// {
+// 	// Statically
+// 	Node n1(1);
+// 	Node *head = &n1;
 
-	Node n2(2);
+// 	Node n2(2);
 
-	Node n3(3);
-	Node n4(4);
-	Node n5(5);
+// 	Node n3(3);
+// 	Node n4(4);
+// 	Node n5(5);
 
-	n1.next = &n2;
-	n2.next = &n3;
-	n3.next = &n4;
-	n4.next = &n5;
+// 	n1.next = &n2;
+// 	n2.next = &n3;
+// 	n3.next = &n4;
+// 	n4.next = &n5;
 
-	print(head);
+// 	print(head);
 
-	print(head);
+// 	print(head);
 	
-  /*
+/*
 	n1.next = &n2;
 	cout << n1.data << " " << n2.data << endl;
 
@@ -66,5 +66,5 @@ int main()
 	
 	Node *n4 = new Node(20);
 	n3 -> next = n4;
-	*/
-}
+*/
+// }
