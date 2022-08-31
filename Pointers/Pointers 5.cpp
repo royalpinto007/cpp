ca19ebb5cd30fdcad955756b301a0cfe44a6177b
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-31 09:20:49 +0530

    Update and rename Pointers5.cpp to Pointers 5.cpp

--- Pointers/Pointers 5.cpp
+++ Pointers/Pointers 5.cpp
@@ -8,4 +8,3 @@ int main()
     *ptr = a;
     cout << *ptr << endl;    // Segmentation error because we are trying to access a memory which is not available or say accessible to us
 }                            // So always initalize pointers with some existing variables to avoid this error
-
