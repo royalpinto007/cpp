f76e4d6b17e8eafd2337c5693e968c34b7c7f676
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-19 00:44:23 +0530

    Update Static Members.cpp

--- OOPS 2/Notes/Static Members.cpp
+++ OOPS 2/Notes/Static Members.cpp
@@ -22,3 +22,22 @@ class Student {
 };
 
 int Student :: totalStudents = 0;	// initialze static data members
+
+/*
+#include <iostream>
+using namespace std;
+#include "Student.cpp"
+
+int main() {
+	Student s1;
+
+	Student s2;
+
+	Student s3, s4, s5;
+
+	// cout << Student :: totalStudents << endl;
+
+	cout << Student :: getTotalStudent() << endl;
+
+}
+*/
