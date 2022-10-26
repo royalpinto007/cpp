cb75809104a8de18c2a2e057d0a8dd2a716cb46f
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-26 08:14:24 +0530

    Update Midpoint of LL.cpp

--- Linked List 2/Midpoint of LL.cpp
+++ Linked List 2/Midpoint of LL.cpp
@@ -1,26 +1,3 @@
-/****************************************************************
- 
-    Following is the class structure of the Node class:
-
-        class Node
-        {
-        public:
-	        int data;
-	        Node *next;
-	        Node(int data)
-	        {
-		        this->data = data;
-		        this->next = NULL;
-	        }
-        };
-
-*****************************************************************/
-/* 
-Time Complexity : O(n) 
-Space Complexity : O(1) 
-where n is the size of singly linked list 
-*/ 
-
 Node *midPoint(Node *head) 
 { 
     if (head == NULL || head->next == NULL) // head==NULL is a condition for odd & head->next == NULL is a condition for even
@@ -38,34 +15,23 @@ Node *midPoint(Node *head)
     return slow; 
 }
 
-// #include <cmath>
-// int length(Node*head)
-// {
-//      int count=1;
-//     while(head)
-//     {
-//         count++;
-//         head=head->next;
-//     }
-//     return count;
-// }
-// Node *midPoint(Node *head)
-// {
-//      if(head==NULL)	 return head;
-//     // Write your code here
-//    int x=ceil(length(head)/2);
-    
-//     for(int i=1;i<x;i++)
-//     {
-//         head=head->next;
-//     }
-  
-//     return head;
-// }
-
 /*
 #include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
 using namespace std;
+#include "solution.h"
 
 Node *takeinput()
 {
