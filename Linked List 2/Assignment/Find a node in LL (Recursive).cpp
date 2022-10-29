b7664929564e473372bcac02c979fef8856449b5
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-29 18:58:01 +0530

    Update Find a node in LL (Recursive).cpp

--- Linked List 2/Assignment/Find a node in LL (Recursive).cpp
+++ Linked List 2/Assignment/Find a node in LL (Recursive).cpp
@@ -1,16 +1,12 @@
 int findNodeRec(Node *head, int n) 
 {
-    if(head==NULL)
-        return -1;
-    if(head->data==n)
-        return 0;
+    if(head==NULL) return -1;
+    if(head->data==n) return 0;
     
     int x=findNodeRec(head->next,n);
-    if(x==-1)
-        return x;
-
-    else
-        return x+1;
+   
+    if(x==-1) return x;
+    else return x+1;
 }
 
 /*
