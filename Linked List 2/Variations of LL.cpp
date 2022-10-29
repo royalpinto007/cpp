f49d2ff2c24b3a255bd3584c724e3547bb45e6e8
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-29 18:46:55 +0530

    Create Variations of LL.cpp

---
+++ Linked List 2/Variations of LL.cpp
@@ -0,0 +1,39 @@
+// Reference- https://www.geeksforgeeks.org/types-of-linked-list/
+
+1) Circular doubly LL
+
+Given an unsorted circular doubly linked list, suppose you have reference (or pointer) to its head node only, which of the following operation can be implemented in O(1) time?
+i) Insertion at the front of the linked list
+ii) Insertion at the end of the linked list
+iii) Deletion of the last node of the linked list
+iv) Deletion of the front node of the linked list
+
+Answer- i, ii, iii, iv
+
+2) Traversal in LL
+In doubly linked lists, traversal can be done in ?
+
+i) Only forward direction
+ii) Only reverse direction
+iii) Both directions
+iv) None of the above
+
+Answer- iii
+
+3) Double LL
+Given an unsorted doubly Linked List, suppose you have references (or pointer) to its head and tail nodes, which of the following operation can be implemented in O(1) time ?
+i) Insertion at the front of the linked list
+ii) Insertion at the end of the linked list
+iii) Deletion of the last node of the linked list
+iv) Deletion of the front node of the linked list
+
+Answer- i, ii, iii, iv
+
+4) Circular LL
+Given an unsorted circular linked list, suppose you have reference (or pointer) to its head node only, which of the following operation can be implemented in O(1) time?
+i) Insertion at the front of the linked list
+ii) Insertion at the end of the linked list
+iii) Deletion of the last node of the linked list
+iv) Deletion of the front node of the linked list
+
+Answer- None
