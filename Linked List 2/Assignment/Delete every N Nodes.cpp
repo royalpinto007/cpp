3f381a2e79485aa3076efc28c5f464d59916b754
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-29 19:13:43 +0530

    Update Delete every N Nodes.cpp

--- Linked List 2/Assignment/Delete every N Nodes.cpp
+++ Linked List 2/Assignment/Delete every N Nodes.cpp
@@ -1,22 +1,3 @@
-/****************************************************************
- 
-    Following is the class structure of the Node class:
-
-        class Node
-        {
-        public:
-	        int data;
-	        Node *next;
-	        Node(int data)
-	        {
-		        this->data = data;
-		        this->next = NULL;
-	        }
-        };
-
-*****************************************************************/
-
-
 Node* skipMdeleteN(Node *head, int M, int N){ 
 
     if(M==0) return NULL; // edge case, not a base case
@@ -55,3 +36,71 @@ Node* skipMdeleteN(Node *head, int M, int N){
     t1->next=m;
     return head;
 }
+
+/*
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+#include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int m, n;
+		cin >> m >> n;
+		head = skipMdeleteN(head, m, n);
+		print(head);
+	}
+	return 0;
+}
+*/
