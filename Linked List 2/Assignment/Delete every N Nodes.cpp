7896ff788eed7ca9d73565da3a35a5da5af4c08c
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-29 19:12:15 +0530

    Update Delete every N Nodes.cpp

--- Linked List 2/Assignment/Delete every N Nodes.cpp
+++ Linked List 2/Assignment/Delete every N Nodes.cpp
@@ -1,16 +1,30 @@
-Node* skipMdeleteN(Node *head, int M, int N) 
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
 
-    if(M==0) return NULL; // edge case  not a base case
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+
+Node* skipMdeleteN(Node *head, int M, int N){ 
+
+    if(M==0) return NULL; // edge case, not a base case
     
     //base case
-    if(head==NULL ||head->next==NULL) 
-    {
-        return head;
-    }
-    if(N==0)
-    {
-        return head;
-    }
+    if(head==NULL ||head->next==NULL) return head;
+    
+    if(N==0) return head;
     
     Node*temp=head;
     int count1=0;
@@ -26,13 +40,12 @@ Node* skipMdeleteN(Node *head, int M, int N)
    	// t1=temp;
   	temp=temp->next;
 
-    if(temp==NULL)
-        return head;
+    if(temp==NULL) return head;
     
     while(temp->next!=NULL && count2<N-1)
-    {   count2++;
+    {   
+        count2++;
         temp=temp->next;
-      
     }
     
     Node*t2;
@@ -42,69 +55,3 @@ Node* skipMdeleteN(Node *head, int M, int N)
     t1->next=m;
     return head;
 }
-
-/*
-#include <iostream>
-using namespace std;
-
-class Node
-{
-public:
-	int data;
-	Node *next;
-	Node(int data)
-	{
-		this->data = data;
-		this->next = NULL;
-	}
-};
-
-Node *takeinput()
-{
-	int data;
-	cin >> data;
-	Node *head = NULL, *tail = NULL;
-	while (data != -1)
-	{
-		Node *newnode = new Node(data);
-		if (head == NULL)
-		{
-			head = newnode;
-			tail = newnode;
-		}
-		else
-		{
-			tail->next = newnode;
-			tail = newnode;
-		}
-		cin >> data;
-	}
-	return head;
-}
-
-void print(Node *head)
-{
-	Node *temp = head;
-	while (temp != NULL)
-	{
-		cout << temp->data << " ";
-		temp = temp->next;
-	}
-	cout << endl;
-}
-
-int main()
-{
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		Node *head = takeinput();
-		int m, n;
-		cin >> m >> n;
-		head = skipMdeleteN(head, m, n);
-		print(head);
-	}
-	return 0;
-}
-*/
