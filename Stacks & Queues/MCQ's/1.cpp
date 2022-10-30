ab4f766f572c4b2eb863d8a594de510dae1ce8e1
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-30 22:25:46 +0530

    Create 1.cpp

---
+++ Stacks & Queues/MCQ's/1.cpp
@@ -0,0 +1,44 @@
+1) A stack is a ?
+  
+FIFO (First In First Out) list
+LIFO (Last In First Out) list.
+Ordered array
+Linear tree
+
+Answer- LIFO
+
+2) Which data structure is used for implementing recursion?
+
+Queue
+Stack
+Array
+List
+
+Answer- Stack
+
+3) Which of the following statement(s) about stack data structure is/are NOT correct?
+  
+Stack data structure can be implemented using linked list
+New node can only be added at the top of the stack
+Stack is the FIFO data structure 
+New node can be added at both ends 
+
+Answer- 3, 4
+  
+4) Which of the following operation take worst case i.e. linear time (O(n)) in the array implementation of stack?
+  
+Push
+Pop
+IsEmpty
+None
+
+Answer- none
+
+5) If the elements “p”, “q”, “r” and “s” are placed in a stack and are deleted one at a time, in what order will they be removed?
+
+srqp
+pqrs
+srpq
+qrsp
+
+Answer- srqp
