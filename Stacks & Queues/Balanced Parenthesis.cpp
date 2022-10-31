5cccdf1f40d7efa156800c5bd790c0713450aaab
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-31 22:20:59 +0530

    Update Balanced Parenthesis.cpp

--- Stacks & Queues/Balanced Parenthesis.cpp
+++ Stacks & Queues/Balanced Parenthesis.cpp
@@ -1,3 +1,25 @@
+/*
+For a given a string expression containing only round brackets or parentheses, check if they are balanced or not. Brackets are said to be balanced if the bracket which opens last, closes first.
+
+Example:
+Expression: (()())
+Since all the opening brackets have their corresponding closing brackets, we say it is balanced and hence the output will be, 'true'.
+You need to return a boolean value indicating whether the expression is balanced or not.
+
+Note:
+The input expression will not contain spaces in between.
+
+Sample Input 1 :
+(()()())
+Sample Output 1 :
+true
+
+Sample Input 2 :
+()()(()
+Sample Output 2 :
+false
+*/
+    
 #include <stack> 
 
 bool isBalanced(string expression) { 
