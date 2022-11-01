5deeaad541975a2cf902c255a9d3b33b95713a5e
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-01 17:40:24 +0530

    Create Check redundant brackets.cpp

---
+++ Stacks & Queues/Assignment/Check redundant brackets.cpp
@@ -0,0 +1,43 @@
+#include <stack> 
+bool checkRedundantBrackets(string expression) 
+{ 
+    stack<char> stk; string operators = "+-*/"; 
+    for (int i = 0; i < expression.length(); ++i) 
+    { 
+        if (expression[i] == '(' || operators.find(expression[i]) != string::npos) 
+        { 
+            stk.push(expression[i]); 
+        } 
+        else if (expression[i] == ')') 
+        { 
+            bool hasOperator = false; 
+            while (!stk.empty() && stk.top() != '(') 
+            { 
+                stk.pop(); hasOperator = true; 
+            } 
+            if (!hasOperator) 
+            {
+                return true; 
+            } 
+            if (!stk.empty()) 
+            { 
+                stk.pop(); 
+            } 
+        }
+    } 
+    return false; 
+}
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "solution.h"
+
+int main() {
+    string input;
+    cin >> input;
+    cout << ((checkRedundantBrackets(input)) ? "true" : "false");
+}
+*/
