a74c396992d914b16c5bad47cd54a7c07ec8fdde
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-01 17:32:44 +0530

    Create 2.cpp

---
+++ Stacks & Queues/MCQ's/2.cpp
@@ -0,0 +1,44 @@
+1) A queue is a ?
+  
+FIFO (First In First Out) list
+LIFO (Last In First Out) list
+Ordered array
+Linear tree
+
+Answer- FIFO
+
+2) If the elements “p”, “q”, “r” and “s” are placed in a queue and are deleted one at a time, in what order will they be removed?
+  
+srqp
+pqrs
+srpq
+qrsp
+
+Answer- pqrs
+
+3) In optimized linked list implementation of a queue, where does a new element be inserted?
+  
+At the head of linked list
+At the tail of the linked list
+At the centre position in the linked list
+None
+
+Answer- At the tail of the linked list
+
+4) In linked list implementation of queue, if only front pointer is maintained, which of the following operation take worst case time (i.e. O(n) )?
+
+Insertion from rear
+Deletion from front
+Both
+None
+
+Answer- Insertion from rear
+
+5) In linked list implementation of a queue, front and rear pointers are tracked. Which of these pointers will change during an insertion into EMPTY queue?
+    
+Only front pointer
+Only rear pointer
+Both front and rear pointer
+None
+
+Answer- Both front and rear pointer
