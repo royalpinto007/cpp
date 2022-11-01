ecebd935d3e2341b36c38bacff079b2b22d08ded
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-01 17:53:15 +0530

    Update and rename Test 2 (DSA).cpp to Test 2 (DSA)/Next Number.cpp

--- Test 2 (DSA)/Next Number.cpp
+++ Test 2 (DSA)/Next Number.cpp
@@ -1,3 +1,18 @@
+// Given a large number represented in the form of a linked list. 
+// Write code to increment the number by 1 in-place(i.e. without using extra space).
+    
+// Note: You don't need to print the elements, just update the elements and return the head of updated LL.
+    
+// Sample Input 1 :
+// 3 9 2 5 -1
+// Sample Output 1 :
+// 3 9 2 6
+
+// Sample Input 2 :
+// 9 9 9 -1
+// Sample Output 1 :
+// 1 0 0 0 
+    
 Node* NextLargeNumber(Node *head) {
     
     if(head->next==NULL)
