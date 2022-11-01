be0f18623d5a4d8dc7627007155df648f519b7a2
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-01 18:10:08 +0530

    Create MCQ.cpp

---
+++ Test 1/MCQ.cpp
@@ -0,0 +1,58 @@
+1) Find the output.
+int p = 100;
+if(p > 20) {
+    if(p < 20) {
+        printf("coding");
+    }
+} else {
+    printf("ninjas");
+}
+
+coding
+ninjas
+codingninjas
+No Output 
+
+Answer- No output
+
+2) Write the correct order(s) of statements.
+  
+We want to print "Coding Ninjas" 10 times.
+1.  while(x <= 10) {
+2.  int x = 1;
+3.  x += 1;
+4.  cout<<"Coding Ninjas"<<endl;
+5.  }
+
+
+2 3 1 4 5
+2 1 3 4 5 
+2 1 4 3 5 
+1 2 4 3 5
+  
+Answer- b, c
+
+3) Figure out the output
+int x = 10;
+while(x >= 0) {
+    x = x - 1;
+    cout<<x;
+    x--;
+}
+
+
+97531
+97531-1 
+10864
+10987654321
+  
+Answer- b
+
+4) Please select the correct statement(s) about && and || operators.
+  
+a && b is true if either a or b is true
+(a || b) && c is true if c is true and either a or b is true 
+a && b is false if both a and b are true
+a || b is true if either a or b is true 
+  
+Answer- b, d
