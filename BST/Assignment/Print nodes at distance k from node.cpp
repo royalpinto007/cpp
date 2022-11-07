e455c1d42cf3e0fad6fc464bd9b48b51e34c70ee
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-07 14:44:36 +0530

    Create Print nodes at distance k from node.cpp

---
+++ BST/Assignment/Print nodes at distance k from node.cpp
@@ -0,0 +1,17 @@
+// You are given a Binary Tree of type integer, a target node, and an integer value K.
+// Print the data of all nodes that have a distance K from the target node. The order in which they would be printed will not matter.
+
+// Sample Input 1:
+// 5 6 10 2 3 -1 -1 -1 -1 -1 9 -1 -1
+// 3 1
+// Sample Output 1:
+// 9
+// 6
+  
+// Sample Input 2:
+// 1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1
+// 3 3
+// Sample Output 2:
+// 4
+// 5
+
