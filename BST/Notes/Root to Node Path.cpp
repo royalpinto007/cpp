7294319eb0afedd0bd3677df72ee0d46156479cc
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-07 13:19:14 +0530

    Create Root to Node Path.cpp

---
+++ BST/Notes/Root to Node Path.cpp
@@ -0,0 +1,26 @@
+vector<int>* getRootToNodePath(BinaryTreeNode<int>* root, int data) {
+  
+	if (root == NULL) return NULL;
+
+	if (root->data == data) {
+		vector<int>* output = new vector<int>();
+		output->push_back(root->data);
+		return output;
+	}
+	
+	vector<int>* leftOutput = getRootToNodePath(root->left, data);
+	if (leftOutput != NULL) {
+		leftOutput->push_back(root->data);
+		return leftOutput;
+	}
+
+	vector<int>* rightOutput = getRootToNodePath(root->right, data);
+  
+	if (rightOutput != NULL) {
+			rightOutput->push_back(root->data);
+			return rightOutput;
+	} 
+  else {
+		return NULL;
+	}
+}
