b4fcefb3163d8ff5c07e474442578b4506ea5807
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-07 14:47:33 +0530

    Update Print nodes at distance k from node.cpp

--- BST/Assignment/Print nodes at distance k from node.cpp
+++ BST/Assignment/Print nodes at distance k from node.cpp
@@ -15,3 +15,119 @@
 // 4
 // 5
 
+void printkdistanceNodeDown(BinaryTreeNode<int> *root, int k){
+
+    if (root == NULL || k < 0) return;
+
+    if (k == 0)
+    {
+        cout << root->data << endl;
+        return;
+    }
+    printkdistanceNodeDown(root->left, k - 1);
+    printkdistanceNodeDown(root->right, k - 1);
+}
+
+int printkdistanceNode(BinaryTreeNode<int> *root, int target, int k)
+{
+    if (root == NULL)
+        return -1;
+    if (root->data == target)
+    {
+        printkdistanceNodeDown(root, k);
+        return 0;
+    }
+
+    int dl = printkdistanceNode(root->left, target, k);
+    if (dl != -1)
+    {
+        if (dl + 1 == k)
+            cout << root->data << endl;
+        else
+            printkdistanceNodeDown(root->right, k - dl - 2);
+        return 1 + dl;
+    }
+    int dr = printkdistanceNode(root->right, target, k);
+    if (dr != -1)
+    {
+        if (dr + 1 == k)
+            cout << root->data << endl;
+        else
+            printkdistanceNodeDown(root->left, k - dr - 2);
+        return 1 + dr;
+    }
+    return -1;
+}
+void nodesAtDistanceK(BinaryTreeNode<int> *root, int node, int k){
+    
+    int x = printkdistanceNode(root, node, k);
+}
+
+/*
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~BinaryTreeNode() {
+    	if(left) 
+       		delete left;
+    	if(right) 
+       		delete right;
+    }
+};
+
+using namespace std;
+#include "solution.h"
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    int targetNode, k;
+    cin >> targetNode >> k;
+    nodesAtDistanceK(root, targetNode, k);
+    delete root;
+}
+*/
