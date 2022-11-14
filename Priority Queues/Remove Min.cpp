20cfcc922fafe519a60cd7dbbc48df0cc2eee903
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-15 02:27:42 +0530

    Update Remove Min.cpp

--- Priority Queues/Remove Min.cpp
+++ Priority Queues/Remove Min.cpp
@@ -54,6 +54,44 @@
 //         }
 //     }
 
+        // below there is another solution
+//     	int removeMin() {
+// 		if(isEmpty()) {
+// 			return 0;		// Priority Queue is empty
+// 		}
+// 		int ans = pq[0];
+// 		pq[0] = pq[pq.size() - 1];
+// 		pq.pop_back();
+
+// 		// down-heapify
+
+// 		int parentIndex = 0;
+// 		int leftChildIndex = 2 * parentIndex + 1;
+// 		int rightChildIndx = 2 * parentIndex + 2;
+	
+// 		while(leftChildIndex < pq.size()) {
+// 			int minIndex = parentIndex;
+// 			if(pq[minIndex] > pq[leftChildIndex]) {
+// 				minIndex = leftChildIndex;
+// 			}
+// 			if(rightChildIndx < pq.size() && pq[rightChildIndx] < pq[minIndex]) {
+// 				minIndex = rightChildIndx;
+// 			}
+// 			if(minIndex == parentIndex) {
+// 				break;
+// 			}
+// 			int temp = pq[minIndex];
+// 			pq[minIndex] = pq[parentIndex];
+// 			pq[parentIndex] = temp;
+		
+// 			parentIndex = minIndex;
+// 			leftChildIndex = 2 * parentIndex + 1;
+// 			rightChildIndx = 2 * parentIndex + 2;
+// 		}
+
+// 		return ans;
+// 	}
+	
     int removeMin()
     {
       
