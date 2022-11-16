8ffa0ffb638078d4a148101ad8734d61229887fa
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-17 00:51:24 +0530

    Update Remove Duplicates.cpp

--- Hashmaps/Notes/Remove Duplicates.cpp
+++ Hashmaps/Notes/Remove Duplicates.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<int> removeDuplicates(int* a, int size) {
 	vector<int> output;
-	unordered_map<int, bool> seen;
+	unordered_map<int, bool> seen;		// <key, value>
 	for (int i = 0; i < size; i++) {
 		if (seen.count(a[i]) > 0) {
 			continue;
