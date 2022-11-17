91c6ec3eebb70191b6f0afb2e667d69821a6633a
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-18 04:35:11 +0530

    Update Min Steps to 1 (Using DP).cpp

--- Dynamic Programming 1/Min Steps to 1 (Using DP).cpp
+++ Dynamic Programming 1/Min Steps to 1 (Using DP).cpp
@@ -22,44 +22,22 @@
 // Step 2 : n = 6  / 3 = 2 
 // Step 3 : n = 2 / 2 = 1  
   
-int countStepsToOne(int n)
-{
-    int *minSteps = new int[n + 1];
-    minSteps[1] = 0;
-    for (int i = 2; i <= n; ++i)
-    {
-        int subtractOne = minSteps[i - 1];
-        int divideByTwo = INT_MAX;
-        int divideByThree = INT_MAX;
-        if (i % 2 == 0)
-        {
-            divideByTwo = minSteps[i / 2];
-        }
-        if (i % 3 == 0)
-        {
-            divideByThree = minSteps[i / 3];
-        }
-        minSteps[i] = 1 + min(subtractOne, min(divideByTwo, divideByThree));
-    }
-    int result = minSteps[n];
-    delete[] minSteps;
-    return result;
-}
-
-/*
 int countStepsToOne(int n){
 
     int *ans = new int[n + 1];
     ans[0] = 0;
     ans[1] = 0;
+  
     for (int i = 2; i <= n; i++)
     {
         int a = INT_MAX, b = INT_MAX, c = INT_MAX;
+      
         if (i % 3 == 0)
             a = ans[i / 3];
         if (i % 2 == 0)
             b = ans[i / 2];
         c = ans[i - 1];
+      
         if (a <= b && a <= c)
             ans[i] = 1 + a;
         else if (b <= a && b <= c)
@@ -69,7 +47,109 @@ int countStepsToOne(int n){
     }
     return ans[n];
 }
-*/
+
+// CN video solution
+// #include <iostream>
+// using namespace std;
+
+// int minStepsHelper(int n, int *ans) {
+// 	// Base case
+// 	if(n <= 1) {
+// 		return 0;
+// 	}
+
+// 	// Check if output already exists
+// 	if(ans[n] != -1) {
+// 		return ans[n];
+// 	}
+
+// 	// Calculate output
+// 	int x = minStepsHelper(n - 1, ans);
+
+// 	int y = INT_MAX, z = INT_MAX;
+// 	if(n % 2 == 0) {
+// 		y = minStepsHelper(n/2, ans);
+// 	}
+
+// 	if(n % 3 == 0) {
+// 		z = minStepsHelper(n/3, ans);
+// 	}
+
+// 	int output = min(x, min(y, z)) + 1;
+
+// 	// Save output for future use
+// 	ans[n] = output;
+
+// 	return output;
+
+// }
+
+// int misSteps_2(int n) {
+// 	int *ans = new int[n+1];
+
+// 	for(int i = 0; i <= n; i++) {
+// 		ans[i] = -1;
+// 	}
+
+// 	return minStepsHelper(n, ans);
+// }
+
+
+// int minSteps(int n) {
+// 	// Base case
+// 	if(n <= 1) {
+// 		return 0;
+// 	}
+
+// 	// Recursive call
+// 	int x = minSteps(n - 1);
+
+// 	int y = INT_MAX, z = INT_MAX;
+// 	if(n % 2 == 0) {
+// 		y = minSteps(n/2);
+// 	}
+
+// 	if(n % 3 == 0) {
+// 		z = minSteps(n/3);
+// 	}
+
+// 	// Calculate final output
+// 	int ans = min(x, min(y, z)) + 1;
+
+// 	return ans;
+// }
+
+// int main() {
+// 	int n;
+// 	cin >> n;
+
+// 	cout << minSteps(n) << endl;
+// }
+
+// CN Solution-
+// int countStepsToOne(int n)
+// {
+//     int *minSteps = new int[n + 1];
+//     minSteps[1] = 0;
+//     for (int i = 2; i <= n; ++i)
+//     {
+//         int subtractOne = minSteps[i - 1];
+//         int divideByTwo = INT_MAX;
+//         int divideByThree = INT_MAX;
+//         if (i % 2 == 0)
+//         {
+//             divideByTwo = minSteps[i / 2];
+//         }
+//         if (i % 3 == 0)
+//         {
+//             divideByThree = minSteps[i / 3];
+//         }
+//         minSteps[i] = 1 + min(subtractOne, min(divideByTwo, divideByThree));
+//     }
+//     int result = minSteps[n];
+//     delete[] minSteps;
+//     return result;
+// }
 
 // #include <bits/stdc++.h>
 // using namespace std;
