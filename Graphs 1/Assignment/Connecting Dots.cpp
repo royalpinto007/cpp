89b83c73f2a9dead54dc8ee266d7b0f5db085b5b
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-21 13:51:54 +0530

    Update Connecting Dots.cpp

--- Graphs 1/Assignment/Connecting Dots.cpp
+++ Graphs 1/Assignment/Connecting Dots.cpp
@@ -116,3 +116,26 @@ bool hasCycle(vector<vector<char>> &board, int n, int m){
     }
     return 0;
 }
+
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "solution.h"
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<char>> board(n, vector<char>(m));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> board[i][j];
+        }
+    }
+
+    cout << (hasCycle(board, n, m) ? "true" : "false");
+}
+*/
