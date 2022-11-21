87dc7f76360322b19b89f52188dc58c0248e4547
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-21 13:40:41 +0530

    Create Has Path.cpp

---
+++ Graphs 1/Has Path.cpp
@@ -0,0 +1,95 @@
+// Given an undirected graph G(V, E) and two vertices v1 and v2 (as integers), check if there exists any path between them or not. 
+// Print true if the path exists and false otherwise.
+// Note:
+// 1. V is the number of vertices present in graph G and vertices are numbered from 0 to V-1. 
+// 2. E is the number of edges present in graph G.
+
+// Sample Input 1 :
+// 4 4
+// 0 1
+// 0 3
+// 1 2
+// 2 3
+// 1 3
+// Sample Output 1 :
+// true
+  
+// Sample Input 2 :
+// 6 3
+// 5 3
+// 0 1
+// 3 4
+// 0 3
+// Sample Output 2 :
+// false
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+bool hasPath(bool **edges, long long v, bool *visited, long long v1, long long v2)
+{
+    if (v2 >= v || v1 >= v)
+        return false;
+    if (v1 == v2)
+        return true;
+    if (edges[v1][v2] || edges[v2][v1])
+        return true;
+        
+    queue<long long> pendingVertices;
+    pendingVertices.push(v1);
+    visited[v1] = true;
+    while (!pendingVertices.empty())
+    {
+        for (long long i = 0; i < v; i++)
+        {
+            if (i == pendingVertices.front())
+                continue;
+            if (visited[i])
+                continue;
+            if (edges[pendingVertices.front()][i])
+            {
+                pendingVertices.push(i);
+                visited[i] = true;
+            }
+        }
+        pendingVertices.pop();
+        if (visited[v2])
+            return true;
+    }
+    return false;
+}
+
+int main(){
+    
+    long long v, e;
+    cin >> v >> e;
+    bool **edges = new bool *[v];
+    for (long long i = 0; i < v; i++)
+    {
+        edges[i] = new bool[v];
+        for (long long j = 0; j < v; j++)
+        {
+            edges[i][j] = false;
+        }
+    }
+    for (long long i = 0; i < e; i++)
+    {
+        long long start, end;
+        cin >> start >> end;
+        edges[start][end] = true;
+        edges[end][start] = true;
+    }
+    bool *visited = new bool[v];
+    for (long long i = 0; i < v; i++)
+    {
+        visited[i] = false;
+    }
+    long long v1, v2;
+    cin >> v1 >> v2;
+    if (hasPath(edges, v, visited, v1, v2))
+        cout << "true";
+    else
+        cout << "false";
+    return 0;
+}
