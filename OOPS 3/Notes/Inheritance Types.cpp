d1cc697937cbf69a399d6245498293136841bd4a
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-11-22 13:14:29 +0530

    Create Inheritance Types.cpp

---
+++ OOPS 3/Notes/Inheritance Types.cpp
@@ -0,0 +1,81 @@
+class Car : public Vehicle {
+	public :
+		int numGears;
+
+		Car(int x, int y) : Vehicle(x) {
+			cout << "Car's constructor " << endl;
+			numGears = y;
+		}
+
+		~Car() {
+			cout << "Car's Destructor " << endl;
+		}
+
+		void print() {
+			cout << "NumTyres : " << numTyres << endl;
+			cout << "Color : " << color << endl;
+			cout << "Num gears : " << numGears << endl;
+			//		cout << "Max Speed : " << maxSpeed << endl;
+    }
+};
+
+class HondaCity : public Car {
+	public :
+
+		HondaCity(int x, int y) : Car(x, y) {
+			cout << "Honda city constructor " << endl;
+		}
+
+		~HondaCity() {
+			cout << "Honda city destructor " << endl;
+
+		}
+};
+
+class Vehicle {
+	private :
+		int maxSpeed;
+
+	protected :
+		int numTyres;
+
+	public :
+		string color;
+
+		/*
+	Vehicle() {
+		cout << "Vehicle's default constructor " << endl;
+	}*/
+
+	Vehicle(int z) {
+		cout << "Vehicle's Parametrized constructor " << endl;
+		maxSpeed = z;
+	}
+
+	~Vehicle() {
+		cout << "Vehicle's Destructor " << endl;
+	}
+};
+
+#include <iostream>
+using namespace std;
+#include "Vehicle.cpp"
+#include "Car.cpp"
+#include "HondaCity.cpp"
+
+int main() {
+
+	HondaCity h(4, 5);
+
+	//	Vehicle v;
+
+	//v.color = "Blue";
+	// v.maxSpeed = 100;
+	// v.numTyres = 4;
+  
+//	Car c(5);
+
+	//c.color = "Black";
+//	c.numTyres = 4;
+	// c.numGears = 5;
+}
